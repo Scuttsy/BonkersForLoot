d4a9189a0e77ae87f03291470e56ea03e8b23a2a
CaTTco2 <nicolas.holemans.pepermans@student.howest.be>
2024-04-25 17:09:20 +0200

    Time remaining visible in scene

--- Assets/Scripts/NicoTest/GameplayScene.cs
+++ Assets/Scripts/NicoTest/GameplayScene.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 // !!! There should be an empty object in the gameplayscene with this script attached. !!!
 // !!! There should be an empty object in the gameplayscene with this script attached. !!!
@@ -9,7 +12,7 @@ using UnityEngine;
 public class GameplayScene : MonoBehaviour
 {
     private float _timeRemaining = 30f; // In Seconds!
-
+    [SerializeField] private TextMeshProUGUI _timerText;
     private void Awake()
     {
         // Create Player list in GameSettings before other script's starts are called
@@ -34,11 +37,22 @@ public class GameplayScene : MonoBehaviour
         if (_timeRemaining < 0)
         {
             //To-do Switch to game over scene
+            GameSettings.GameIsInProgress = false;
             Debug.Log("Game over");
         }
         else
         {
             _timeRemaining -= Time.deltaTime;
         }
+
+        DisplayTime();
+    }
+
+    private void DisplayTime()
+    {
+        float minutes = Mathf.FloorToInt(_timeRemaining / 60);
+        float seconds = Mathf.FloorToInt(_timeRemaining % 60);
+
+        _timerText.text = $"{minutes}:{seconds}";
     }
 }
