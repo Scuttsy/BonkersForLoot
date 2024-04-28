ba0ace99d9b458377cfb5750111e53d879a02be8
Scuttsy <scutts001@gmail.com>
2024-04-28 17:16:54 +0200

    Fixed some local issues

---
+++ Assets/Scripts/MapTest/MapBehaviour.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBehaviour : MonoBehaviour
+{
+
+    ////TODO
+    ///SpinningPlatforms 
+    /// <summary>
+    /// The centre bridges surrounding the dunk zone must spin
+    /// The circular platforms must also spin
+    /// </summary>
+    /// Columns must go up and down without affecting the Y position of the player on collisions
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
